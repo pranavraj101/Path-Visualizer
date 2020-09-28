ad1d7b53f180d7b79bafe901d50b7e31bbf78769
pranavraj101 <sinhapranavraj10142@gmail.com>
2020-09-28 09:43:59 +0530

    Project

---
+++ src/tweet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Tweet()
+{
+return(
+    <div>
+        <h3>Name</h3>
+        <p>This is a random tweet</p>
+        <h3>Number of tweets</h3>
+    </div>
+    );
+
+}
+
+export default Tweet;
